34fbc9cad3e1d036c0eb663561ae274b20608539
chasmk <864814035@qq.com>
2024-03-05 02:13:59 +0800

    A1T1 本地和世界坐标的变换

--- src/scene/transform.cpp
+++ src/scene/transform.cpp
@@ -13,14 +13,26 @@ Mat4 Transform::local_to_world() const {
 	// A1T1: local_to_world
 	//don't use Mat4::inverse() in your code.
 
-	return Mat4::I; //<-- wrong, but here so code will compile
+	if (std::shared_ptr<Transform> parent_ = parent.lock()) {
+		//此时该transform有parent
+		return  parent_->local_to_world() * local_to_parent();
+	} else {
+		// 没有parent
+		return local_to_parent();
+	}
 }
 
 Mat4 Transform::world_to_local() const {
 	// A1T1: world_to_local
 	//don't use Mat4::inverse() in your code.
-
-	return Mat4::I; //<-- wrong, but here so code will compile
+	
+	if (std::shared_ptr<Transform> parent_ = parent.lock()) {
+		//此时该transform有parent
+		return  parent_to_local() * parent_->world_to_local();
+	} else {
+		// 没有parent
+		return parent_to_local();
+	}
 }
 
 bool operator!=(const Transform& a, const Transform& b) {
